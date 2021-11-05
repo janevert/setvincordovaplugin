0f4897d1acbd19c41bd1c1d43e6617a9abf54ddf
joaoavsantos <42188271+joaoavsantos@users.noreply.github.com>
2021-11-05 18:03:33 +0100

    Update setvin.js

--- www/setvin.js
+++ www/setvin.js
@@ -2,6 +2,6 @@ var exec = require('cordova/exec');
 
 var PLUGIN_NAME = 'SetVinPlugin';
 
-exports.getVin = function (newVin, success, error) {
+exports.setVin = function (newVin, success, error) {
   exec(success, error, PLUGIN_NAME, "setVin", [newVin]);
 };
